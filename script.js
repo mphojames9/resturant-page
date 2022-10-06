fd49887044cddbd5e9ed6166065c32a920ecc73d
mphojames9 <98138271+mphojames9@users.noreply.github.com>
2022-10-06 15:26:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//Elements
+const homeSection = document.getElementById('home-content');
+const menuSection = document.getElementById('menu-content');
+const contactSection = document.getElementById('contact-content');
+
+function show1(){
+    homeSection.style.display = 'block';
+    menuSection.style.display = 'none';
+    contactSection.style.display = 'none';
+}
+
+function show2(){
+    homeSection.style.display = 'none';
+    menuSection.style.display = 'block';
+    contactSection.style.display = 'none';
+}
+
+function show3(){
+    homeSection.style.display = 'none';
+    menuSection.style.display = 'none';
+    contactSection.style.display = 'block';
+}
